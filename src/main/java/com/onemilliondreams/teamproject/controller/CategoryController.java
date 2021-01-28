f5c0cce703fd1ea7aef75b2d96f193013259b8fa
soyoung201 <soyoung201@naver.com>
2021-01-28 20:11:35 +0900

    commit

--- src/main/java/com/onemilliondreams/teamproject/controller/CategoryController.java
+++ src/main/java/com/onemilliondreams/teamproject/controller/CategoryController.java
@@ -1,5 +1,18 @@
 package com.onemilliondreams.teamproject.controller;
 
-public class CategoryController {
+import javax.annotation.Resource;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/category")
+public class CategoryController {
+	private static Logger logger = LoggerFactory.getLogger(CategoryController.class);
+	/*
+	 * @Resource private CategoryService categoryService;
+	 */
+	
 }
