6d9f3c7e8a07f506e97531020186787d256b33ee
kymkim96 <kymkim96@naver.com>
2021-02-01 20:54:38 +0900

    할인율 재수정

--- src/main/java/com/onemilliondreams/teamproject/controller/CartPageController.java
+++ src/main/java/com/onemilliondreams/teamproject/controller/CartPageController.java
@@ -90,7 +90,12 @@ public class CartPageController {
 				}
 				
 				// 할인가격
-				sumFinalPrice += cartItem.getBfinalPrice() * cartItem.getCtcount();
+				if (cartItem.getBfinalPrice() == 0) {
+					double discount = cartItem.getCtprice() - cartItem.getCtprice() * ((double)cartItem.getCtdiscount()/100);
+					sumFinalPrice += discount * cartItem.getCtcount();
+				} else {
+					sumFinalPrice += cartItem.getBfinalPrice() * cartItem.getCtcount();
+				}
 			}
 			String formattedAmount = NumberFormat.getInstance(Locale.KOREA).format(sumAmount);
 			size = cartItems.size();
